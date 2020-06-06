53dc8ccd36aaac5c608c059017db893e13847fd6
= <steyn.van.l@gmail.com>
2020-06-06 03:33:02 +0200

    Added code from re1 that we need Small modifications to main.c and regexp.c Style changes

---
+++ Makefile
@@ -0,0 +1,30 @@
+# Copyright 2007-2009 Russ Cox.  All Rights Reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+CC=gcc
+CFLAGS=-ggdb -Wall -O2
+
+TARG=re
+OFILES=\
+	compile.o\
+	main.o\
+	pike.o\
+	sub.o\
+	y.tab.o\
+
+HFILES=\
+	regexp.h\
+	y.tab.h\
+
+re: $(OFILES)
+	$(CC) -o re $(OFILES)
+
+%.o: %.c $(HFILES)
+	$(CC) -c $(CFLAGS) $*.c
+
+y.tab.h y.tab.c: parse.y
+	bison -v -y parse.y
+
+clean:
+	rm -f *.o core re y.tab.[ch] y.output
---
+++ main.c
@@ -0,0 +1,65 @@
+// Copyright 2007-2009 Russ Cox.  All Rights Reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "regexp.h"
+
+void
+usage(void)
+{
+	fprintf(stderr, "usage: re regexp string...\n");
+	exit(2);
+}
+
+int
+main(int argc, char **argv)
+{
+    int i, k, l;
+	Regexp *re;
+	Prog *prog;
+	char *sub[MAXSUB];
+
+    if (argc < 2)
+		usage();
+
+	re = parse(argv[1]);
+	printre(re);
+	printf("\n");
+
+    prog = compile(re);
+    printprog(prog);
+
+    for (i = 2; i < argc; i++) {
+        printf("#%d %s\n", i-1, argv[i]);
+        memset(sub, 0, sizeof sub);
+
+        if (!pikevm(prog, argv[i], sub, nelem(sub))) {
+            printf("-no match-\n");
+            continue;
+        }
+
+        printf("match");
+        for (k = MAXSUB; k > 0; k--) {
+            if (sub[k-1]) break;
+        }
+
+        for (l = 0; l < k; l += 2) {
+            printf(" (");
+            if (sub[l] == nil)
+                printf("?");
+            else
+                printf("%d", (int)(sub[l] - argv[i]));
+
+            printf(",");
+            if (sub[l+1] == nil)
+                printf("?");
+            else
+                printf("%d", (int)(sub[l+1] - argv[i]));
+            printf(")");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
